5f4bcd172be860a52eb71c00f5c6c71f3a0e3cc9
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-03-06 11:39:33 +0530

    Update test_tdd.cpp

--- test_tdd.cpp
+++ test_tdd.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
+#include "isContinousRangeDetected.h"
 
 TEST_CASE("Check the continous range is detected") {
   int test_array[] = {3, 3, 5, 4, 10, 11, 12}
